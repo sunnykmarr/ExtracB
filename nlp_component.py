88ce633a8509908a8aa1a0c606b8b4e9aa80ca37
ShashankS <shashank.singhal@jda.com>
2019-03-01 13:10:15 +0530

    - Changes from Sunny

--- nlp_component.py
+++ nlp_component.py
@@ -8,16 +8,12 @@ from flask_cors import CORS
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# import azure.cognitiveservices.speech as speechsdk
-
-# speech_key, service_region = "588aa80d706d4b869a85543562160a3a", "westus"
-# speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-# speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
-
 app = Flask(__name__)
 CORS(app)
 
 inp_file_df = None
+updated_inp_file_df = None
+flag_df_changed = False
 
 
 def preprocessing_input_file():
@@ -28,70 +24,6 @@ def preprocessing_input_file():
 
 
 preprocessing_input_file()
-
-
-def create_speech():
-    return "Say something..."
-    # result = speech_recognizer.recognize_once()
-    # if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-    #     print("Recognized: {}".format(result.text))
-    # elif result.reason == speechsdk.ResultReason.NoMatch:
-    #     print("No speech could be recognized: {}".format(result.no_match_details))
-    # elif result.reason == speechsdk.ResultReason.Canceled:
-    #     cancellation_details = result.cancellation_details
-    #     print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-    #     if cancellation_details.reason == speechsdk.CancellationReason.Error:
-    #         print("Error details: {}".format(cancellation_details.error_details))
-    #
-    # return format(result.text).lower()
-
-
-# def get_speech(speech_txt):
-#     access_token = None
-#     tts = speech_txt
-#     timestr = time.strftime("%Y%m%d-%H%M")
-#     fetch_token_url = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-#     headers = {"Ocp-Apim-Subscription-Key": speech_key}
-#     response = requests.post(fetch_token_url, headers=headers)
-#     access_token = str(response.text)
-#
-#     base_url = "https://westus.tts.speech.microsoft.com/"
-#     path = "cognitiveservices/v1"
-#     constructed_url = base_url + path
-#     headers = {
-#         "Authorization": "Bearer " + str(access_token),
-#         "Content-Type": "application/ssml+xml",
-#         "X-Microsoft-OutputFormat": "riff-24khz-16bit-mono-pcm",
-#         "User-Agent": "YOUR_RESOURCE_NAME",
-#     }
-#     xml_body = ElementTree.Element("speak", version="1.0")
-#     xml_body.set("{http://www.w3.org/XML/1998/namespace}lang", "en-us")
-#     voice = ElementTree.SubElement(xml_body, "voice")
-#     voice.set("{http://www.w3.org/XML/1998/namespace}lang", "en-US")
-#     voice.set("name", "Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)")
-#     voice.text = tts
-#     body = ElementTree.tostring(xml_body)
-#     response = requests.post(constructed_url, headers=headers, data=body)
-#     if response.status_code == 200:
-#         with open("sample-" + timestr + ".wav", "wb") as audio:
-#             audio.write(response.content)
-#             print(
-#                 "\nStatus code: "
-#                 + str(response.status_code)
-#                 + "\nYour TTS is ready for playback.\n"
-#             )
-#             global audio_name
-#             audio_name = str("sample-" + timestr + ".wav")
-#             print(audio_name)
-#
-#     else:
-#         print(
-#             "\nStatus code: "
-#             + str(response.status_code)
-#             + "\nSomething went wrong. Check your subscription key and headers.\n"
-#         )
-#
-#     return audio_name
 
 
 def initializing_bot():
@@ -116,7 +48,7 @@ def initializing_bot():
     return 0
 
 
-def data_normalization(inp_file_df, example_sent):
+def data_normalization(example_sent):
     newStopWords = {"give", "me", "tell", "show", "?"}
     stop_words = set(stopwords.words("english"))
     stop_words = stop_words.union(newStopWords)
@@ -127,137 +59,122 @@ def data_normalization(inp_file_df, example_sent):
     for w in word_tokens:
         if w not in stop_words:
             filtered_sentence.append(w)
-    print("word tokens are", word_tokens)
-    print("filtered sentence tokens are", filtered_sentence)
+    # print("word tokens are", word_tokens)
+    # print("filtered sentence tokens are", filtered_sentence)
     return filtered_sentence
 
 
 @app.route("/")
 def running_bot():
     global inp_file_df
-    while 1:
-        example_col = inp_file_df.columns
-        # Taking user query.
-        # example_sent = "give me the price of item cold coffee on date 25th feb"
-        msg = request.args.get("msg")
-        if msg is None:
-            inp = input("Do you want Typing? yes/ no")
-            if inp == "yes":
-                msg = input("Enter the input")
-            else:
-                msg = create_speech()
-        print("Processing message: {}".format(msg))
-        filtered_sentence = data_normalization(inp_file_df, msg)
+    global updated_inp_file_df
+    global flag_df_changed
+    # print("example columns are", example_col)
+    # Taking user query.
+    # example_sent = "give me the price of item cold coffee on date 25th feb"
+    msg = request.args.get("msg")
+    msg = msg.lower()
+    msg = msg.strip('.')
+    if msg == "yes":
+        flag_df_changed = True
+        return "Please provide the query"
 
-        flag = False
-        for i in range(0, len(filtered_sentence)):
-            if filtered_sentence[i] not in example_col:
-                i += 1
-            else:
-                flag = True
-                break
+    if not flag_df_changed:
+        updated_inp_file_df = inp_file_df
 
-        if not flag:
-            print(
-                "Given columns not found in input file. Please provide correct column names"
-            )
-            break
+    flag_df_changed = False
 
-        allowed_operations = ["range", "sum", "maximum", "minimum", "average"]
-        operation = -1
-        for j in range(0, len(allowed_operations)):
-            if allowed_operations[j] in filtered_sentence[0:i]:
-                operation = j
+    example_col = updated_inp_file_df.columns
+    print("Processing message: {}".format(msg))
+    filtered_sentence = data_normalization(msg)
 
-        y = filtered_sentence[i]
-        x = {}
-        j = i + 1
-        temp_key = ""
-        temp_value = ""
-        flag = False
-        for i in range(j, len(filtered_sentence)):
-            if filtered_sentence[i] in example_col:
-                flag = True
-                if temp_value:
-                    x[temp_key.strip()] = temp_value.strip()
-                    temp_key = ""
-                    temp_value = ""
-                temp_key = filtered_sentence[i]
-            elif flag:
-                temp_value = temp_value + " " + filtered_sentence[i]
+    flag = False
+    for i in range(0, len(filtered_sentence)):
+        if filtered_sentence[i] not in example_col:
             i += 1
-
-        x[temp_key.strip()] = temp_value.strip()
-        print("x value is", x)
-
-        # df.loc[df['column_name'] == some_value]
-        print(inp_file_df.head())
-        query_output = inp_file_df
-        for i in x.keys():
-            query_output = query_output[query_output[i] == x[i]]
-        # query_output = inp_file_df[inp_file_df['item'] == "Cold Coffee"]
-
-        if operation == -1:
-            print(
-                "There are",
-                len(query_output),
-                "matching rows found based on your query",
-            )
-            # print(query_output, row.names = FALSE)
-            print("Query output is", query_output[y].head(1).to_string(index=False))
-
-            inp = "no"
-            if len(query_output) > 1:
-                print(
-                    "Do you want to apply more query on these",
-                    len(query_output),
-                    "rows. Type yes/no",
-                )
-                inp = input()
-                inp_file_df = query_output
-            if inp != "yes":
-                break
-        elif operation == 0:
-            try:
-                print(query_output[y].min(), " to ", query_output[y].max())
-            except:
-                print(
-                    "Range operation is not allowed on non numeric data. Please run the different query"
-                )
-            break
-        elif operation == 1:
-            try:
-                print(query_output[y].sum())
-            except:
-                print(
-                    "Sum operation is not allowed on non numeric data. Please run the different query"
-                )
-            break
-        elif operation == 2:
-            try:
-                print(query_output[y].max())
-            except:
-                print(
-                    "Max operation is not allowed on non numeric data. Please run the different query"
-                )
-            break
-        elif operation == 3:
-            try:
-                print(query_output[y].min())
-            except:
-                print(
-                    "Min operation is not allowed on non numeric data. Please run the different query"
-                )
-            break
         else:
-            try:
-                print(query_output[y].mean())
-            except:
-                print(
-                    "Average operation is not allowed on non numeric data. Please run the different query"
-                )
+            flag = True
             break
-    return 0
+
+    if not flag:
+        res = "Given columns not found in input file. Please provide correct column names"
+        print(res)
+
+    allowed_operations = ["range", "sum", "maximum", "minimum", "average"]
+    operation = -1
+    for j in range(0, len(allowed_operations)):
+        if allowed_operations[j] in filtered_sentence[0:i]:
+            operation = j
+
+    # print("i value is", i)
+    y = filtered_sentence[i]
+    x = {}
+    j = i + 1
+    temp_key = ""
+    temp_value = ""
+    flag = False
+    for i in range(j, len(filtered_sentence)):
+        if filtered_sentence[i] in example_col:
+            flag = True
+            if temp_value:
+                x[temp_key.strip()] = temp_value.strip()
+                temp_value = ""
+            temp_key = filtered_sentence[i]
+        elif flag:
+            temp_value = temp_value + " " + filtered_sentence[i]
+        i += 1
+
+    x[temp_key.strip()] = temp_value.strip()
+    # print("x value is", x)
+
+    # df.loc[df['column_name'] == some_value]
+    # print(updated_inp_file_df.head())
+    query_output = updated_inp_file_df
+    for i in x.keys():
+        query_output = query_output[query_output[i] == x[i]]
+    # query_output = inp_file_df[inp_file_df['item'] == "Cold Coffee"]
+
+    if operation == -1:
+        query_output_string = "There are " + str(
+            len(query_output)) + " matching rows found based on your query." + "\n" + "Query output is " + query_output[
+                                  y].head(1).to_string(index=False) + " ."
+        print(query_output_string)
+        res = query_output_string
+
+        # inp = "no"
+        if len(query_output) > 1:
+            more_query_string = "Do you want to apply more query on these rows. Type yes/no."
+            print(more_query_string)
+            res += "\n" + more_query_string
+            updated_inp_file_df = query_output
+            # res = "Do you want to apply more query on these rows. Type yes/no"
+    elif operation == 0:
+        try:
+            res = str(query_output[y].min()) + " to " + str(query_output[y].max())
+        except:
+            res = "Range operation is not allowed on non numeric data. Please run the different query."
+    elif operation == 1:
+        try:
+            res = str(query_output[y].sum())
+        except:
+            res = "Sum operation is not allowed on non numeric data. Please run the different query."
+    elif operation == 2:
+        try:
+            res = str(query_output[y].max())
+        except:
+            res = "Max operation is not allowed on non numeric data. Please run the different query."
+    elif operation == 3:
+        try:
+            res = str(query_output[y].min())
+        except:
+            res = "Min operation is not allowed on non numeric data. Please run the different query."
+    else:
+        try:
+            res = str(query_output[y].mean())
+        except:
+            res = "Average operation is not allowed on non numeric data. Please run the different query."
+    print(res)
+    return res
 
 
 # adding_stop_words()
